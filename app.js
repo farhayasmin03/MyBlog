609daacf66d48cc02327a379d483922594d5e332
Farhana Yasmin <farhayasmin03@gmail.com>
2019-01-29 10:14:57 +0530

    WIP

--- app.js
+++ app.js
@@ -1,30 +1,44 @@
-var express = require('express');
+    var express = require('express');
 
-var app = express();
-app.set('view engine', 'ejs');
-const bodyparser = require('body-parser');
+    var app = express();
+    app.set('view engine', 'ejs');
+    const bodyparser = require('body-parser');
 
-app.use(bodyparser.urlencoded({
-    extended: true
-}));
+    app.use(bodyparser.urlencoded({
+        extended: true
+    }));
 
-app.use(bodyparser.json());
+    app.use(bodyparser.json());
+    var routes = require('./routes/router')(passport);
+    var session = require('express-session')
+    var passport = require('passport');
+    const LocalStrategy = require('passport-local').Strategy;
 
-var routes = require('./routes/router');
-app.use('/', routes);
-app.use(express.static(__dirname + '/public'));
+    //use sessions for tracking logins
+    app.use(session({
+        secret: 'work hard',
+        resave: true,
+        saveUninitialized: false,
 
-var mongoose=require('mongoose');
-mongoose.connect('mongodb://localhost/blogdb')
-var db= mongoose.connection
+    }));
+    app.use(passport.initialize());
+    app.use(passport.session());
 
-//handling error
-db.on('error',console.error.bind(console,'connection error'));
-db.once('open',function(){
-    console.log('we are connected')
-});
 
-app.listen(3000, function () {
-    console.log('Example app listening on port 3000!')
-});
-module.exports = app;
+    app.use('/', routes);
+    app.use(express.static(__dirname + '/public'));
+
+    var mongoose = require('mongoose');
+    mongoose.connect('mongodb://localhost/blogdb')
+    var db = mongoose.connection
+
+    //handling error
+    db.on('error', console.error.bind(console, 'connection error'));
+    db.once('open', function () {
+        console.log('we are connected')
+    });
+
+    app.listen(3000, function () {
+        console.log('Example app listening on port 3000!')
+    });
+    module.exports = app;
--- routes/router.js
+++ routes/router.js
@@ -1,40 +1,81 @@
-var express = require('express');
-var router = express.Router();
-var Blogs = require('../modules/blogDetails');
-router.get('/', function (req, res) {
-    res.render('index');
-});
-router.get('/new_post', function (req, res) {
-    res.render('new_post')
-});
-router.get('/signUp', function (req, res) {
-    res.render('signUp');
-});
-router.get('/login', function (req, res) {
-    res.render('login')
-});
-router.post('/signUp', function (req, res) {
-    let email = req.body.email;
-    let password = req.body.password;
-    let username = req.body.username;
-    let inputCity = req.body.inputCity;
-    
-    let user = new Blogs({
-        email: email,
-        password:password,
-        username:username,
-        inputCity:inputCity
+    var express = require('express');
+    var router = express.Router();
+    var Blogs = require('../modules/blogDetails');
+    module.exports = function (passport) {
+        router.get('/', function (req, res) {
+            res.render('index');
+        });
+        router.get('/new_post', function (req, res) {
+            res.render('new_post')
+        });
+        router.get('/signUp', function (req, res) {
+            res.render('signUp');
+        });
+        router.get('/login', function (req, res) {
+            res.render('login')
+        });
+        router.post('/signUp', function (req, res) {
+            let email = req.body.email;
+            let password = req.body.password;
+            let username = req.body.username;
+            let inputCity = req.body.inputCity;
+
+            let user = new Blogs({
+                email: email,
+                password: password,
+                username: username,
+                inputCity: inputCity
 
 
-    })
-    user.save((err, savedInstance) => {
-        console.log(err,savedInstance);
-        res.render('new_post');
+            })
+            user.save((err, savedInstance) => {
+                console.log(err, savedInstance);
+                res.render('new_post');
 
-    });
+            });
+
+        });
+        router.post('/login', function (req, res) {
+            passport.authenticate('local', {
+
+                successRedirect: '/new_post',
+                failureRedirect: '/',
+                failureFlash: true
+            });
+            passport.serializeUser(function (user, done) {
+                done(null, user.id);
+            });
+
+            passport.deserializeUser(function (id, done) {
+                User.findById(id, function (err, user) {
+                    done(err, user);
+                });
+            });
+            passport.use(new LocalStrategy(
+                function (username, password, done) {
+                    User.findOne({
+                        username: username
+                    }, function (err, user) {
+                        if (err) {
+                            return done(err);
+                        }
+                        if (!user) {
+                            return done(null, false, {
+                                message: 'Incorrect username.'
+                            });
+                        }
+                        if (!user.validPassword(password)) {
+                            return done(null, false, {
+                                message: 'Incorrect password.'
+                            });
+                        }
+                        return done(null, user);
+                    });
+                }
+            ));
 
 
+        });
+        return router;
 
-});
-
-module.exports = router;
+    }
